3f672012e44f856145352ac9cafdf73d91fa90e1
Aniket <aniket.d2402@gmail.com>
2021-05-16 15:57:16 -0400

    Show Todo message with Delete button

---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+const Todo = (props) => {
+  function deleteHandler() {
+    console.log("Clicked here!");
+  }
+
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
