8b493bc8a664451ee9304799cbd9b6f8c21084d8
Aniket D <aniket.d2402@outlook.com>
2021-05-17 21:34:46 -0400

    code cleanup for new project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import Todo from './components/Todo';
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text='Learn React' />
-      <Todo text='Master React' />
-      <Todo text='Explore React' />
-      
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
