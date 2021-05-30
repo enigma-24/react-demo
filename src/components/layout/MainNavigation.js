3556316702fff1a2699c30a86433747ba09bd9a0
Aniket D <aniket.d2402@outlook.com>
2021-05-29 20:55:31 -0400

    Show My Favourites and their count

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,23 +1,35 @@
+import { useContext } from 'react';
+
+import FavouritesContext from '../../store/favourites-context';
 import { Link } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 
-const MainNavigation = () => (
-  <header className={classes.header}>
-    <div className={classes.logo}>React Meetups</div>
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>All Meetups</Link>
-        </li>
-        <li>
-          <Link to='/new-meetup'>Add NewMeetup</Link>
-        </li>
-        <li>
-          <Link to='/favourites'>My Favourites</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const MainNavigation = () => {
+  const favContext = useContext(FavouritesContext);
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>All Meetups</Link>
+          </li>
+          <li>
+            <Link to='/new-meetup'>Add NewMeetup</Link>
+          </li>
+          <li>
+            <Link to='/favourites'>
+              My Favourites{' '}
+              <span className={classes.badge}>
+                {favContext.totalFavourites}
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default MainNavigation;
--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,3 +1,23 @@
-const FavouritesPage = () => <div>Favourites page</div>;
+import { useContext } from 'react';
+
+import FavouritesContext from '../store/favourites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
+const FavouritesPage = () => {
+  const favContext = useContext(FavouritesContext);
+
+  let content;
+
+  if (favContext.totalFavourites === 0)
+    content = <p>You have no favourites! Start by adding some!</p>;
+  else content = <MeetupList meetups={favContext.favourites} />;
+
+  return (
+    <section>
+      <h1>My Favourites</h1>
+      {content}
+    </section>
+  );
+};
 
 export default FavouritesPage;
